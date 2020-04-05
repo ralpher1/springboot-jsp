a914e8f0e7337f82dc66cde8a93de25b04cfc5a3
Ubuntu <ubuntu@ip-172-31-26-70.ec2.internal>
2020-04-05 23:56:00 +0000

    spring added with react smae time

---
+++ src/main/webapp/precache-manifest.6894a39c7134e2c57b47884ac81bb79d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6545535b77d43d3aad97f1a0a34f12",
+    "url": "./index.html"
+  },
+  {
+    "revision": "71f69765bc8df7385bb0",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4400ce859d5259bdee10",
+    "url": "./static/js/2.4443b2a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4443b2a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71f69765bc8df7385bb0",
+    "url": "./static/js/main.5febf9e5.chunk.js"
+  },
+  {
+    "revision": "651d855aa775d0fa7ee1",
+    "url": "./static/js/runtime-main.b9b476b5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/main/webapp/precache-manifest.721fac44f6168c4354e9d72fee18d37b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1449399149b0e85d7a4ad42618b8f796",
+    "url": "/hello-springboot/index.html"
+  },
+  {
+    "revision": "98e62c29adfa18afa0bc",
+    "url": "/hello-springboot/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4400ce859d5259bdee10",
+    "url": "/hello-springboot/static/js/2.4443b2a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello-springboot/static/js/2.4443b2a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98e62c29adfa18afa0bc",
+    "url": "/hello-springboot/static/js/main.8ef76eab.chunk.js"
+  },
+  {
+    "revision": "c4af585de7dc0d4f7b35",
+    "url": "/hello-springboot/static/js/runtime-main.cef3d8d0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-springboot/static/media/logo.5d5d9eef.svg"
+  }
+]);
